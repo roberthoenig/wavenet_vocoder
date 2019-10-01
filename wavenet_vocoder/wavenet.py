243c6f3bd3640593df034de535dd781be20be90c
Robert Hönig <robhoenig@gmail.com>
2019-10-01 22:39:32 +0100

    Get wavenet vocoder ready for bits-back.
    Adapt incremental_forward to return the probability distribution
    of a given input.
    
    Add incremental_forward_recover to decompress/recover the input.

--- wavenet_vocoder/wavenet.py
+++ wavenet_vocoder/wavenet.py
@@ -105,6 +105,8 @@ class WaveNet(nn.Module):
                  cin_channels=-1, gin_channels=-1, n_speakers=None,
                  weight_normalization=True,
                  upsample_conditional_features=False,
+                 upsample_strides=None,
+                 upsample_kernel_sizes=None,
                  upsample_scales=None,
                  freq_axis_kernel_size=3,
                  scalar_input=False,
@@ -155,16 +157,28 @@ class WaveNet(nn.Module):
         # Upsample conv net
         if upsample_conditional_features:
             self.upsample_conv = nn.ModuleList()
-            for s in upsample_scales:
-                freq_axis_padding = (freq_axis_kernel_size - 1) // 2
-                convt = ConvTranspose2d(1, 1, (freq_axis_kernel_size, s),
-                                        padding=(freq_axis_padding, 0),
-                                        dilation=1, stride=(1, s),
-                                        weight_normalization=weight_normalization)
-                self.upsample_conv.append(convt)
-                # assuming we use [0, 1] scaled features
-                # this should avoid non-negative upsampling output
-                self.upsample_conv.append(nn.ReLU(inplace=True))
+            if upsample_scales is not None:
+                for s in upsample_scales:
+                    freq_axis_padding = (freq_axis_kernel_size - 1) // 2
+                    convt = ConvTranspose2d(1, 1, (freq_axis_kernel_size, s),
+                                            padding=(freq_axis_padding, 0),
+                                            dilation=1, stride=(1, s),
+                                            weight_normalization=weight_normalization)
+                    self.upsample_conv.append(convt)
+                    # assuming we use [0, 1] scaled features
+                    # this should avoid non-negative upsampling output
+                    self.upsample_conv.append(nn.ReLU(inplace=True))
+            else:
+                for stride, upsample_kernel_size in zip(upsample_strides, upsample_kernel_sizes):
+                    freq_axis_padding = (freq_axis_kernel_size - 1) // 2
+                    convt = ConvTranspose2d(1, 1, (freq_axis_kernel_size, upsample_kernel_size),
+                                            padding=(freq_axis_padding, 0),
+                                            dilation=1, stride=(1, stride),
+                                            weight_normalization=weight_normalization)
+                    self.upsample_conv.append(convt)
+                    # assuming we use [0, 1] scaled features
+                    # this should avoid non-negative upsampling output
+                    self.upsample_conv.append(nn.ReLU(inplace=True))
         else:
             self.upsample_conv = None
 
@@ -236,9 +250,9 @@ class WaveNet(nn.Module):
 
         return x
 
-    def incremental_forward(self, initial_input=None, c=None, g=None,
-                            T=100, test_inputs=None,
-                            tqdm=lambda x: x, softmax=True, quantize=True,
+    def incremental_forward(self, all_x, initial_input=None, c=None, g=None,
+                            T=None, test_inputs=None,
+                            tqdm=lambda x: x, softmax=False, quantize=False,
                             log_scale_min=-7.0):
         """Incremental forward step
 
@@ -255,16 +269,16 @@ class WaveNet(nn.Module):
             tqdm (lamda) : tqdm
             softmax (bool) : Whether applies softmax or not
             quantize (bool): Whether quantize softmax output before feeding the
-              network output to input for the next time step. TODO: rename
+                network output to input for the next time step. TODO: rename
             log_scale_min (float):  Log scale minimum value.
 
         Returns:
             Tensor: Generated one-hot encoded samples. B x C x T　
-              or scaler vector B x 1 x T
+                or scaler vector B x 1 x T
         """
         self.clear_buffer()
         B = 1
-
+        T=all_x.size(-1)
         # Note: shape should be **(B x T x C)**, not (B x C x T) opposed to
         # batch forward due to linealized convolution
         if test_inputs is not None:
@@ -290,7 +304,7 @@ class WaveNet(nn.Module):
                 # (B x gin_channels, 1)
                 g = g.transpose(1, 2)
                 assert g.dim() == 3
-        g_btc = _expand_global_features(B, T, g, bct=False)
+    #     g_btc = _expand_global_features(B, T, g, bct=False)
 
         # Local conditioning
         if c is not None and self.upsample_conv is not None:
@@ -320,12 +334,154 @@ class WaveNet(nn.Module):
 
         current_input = initial_input
 
-        for t in tqdm(range(T)):
+        for t in tqdm(range(all_x.size(-1))):
+            if t % 40 == 0:
+                print("iteration", t)
             if test_inputs is not None and t < test_inputs.size(1):
                 current_input = test_inputs[:, t, :].unsqueeze(1)
             else:
                 if t > 0:
-                    current_input = outputs[-1]
+                    current_input = all_x[:, :, t-1]
+
+            # Conditioning features for single time step
+            ct = None if c is None else c[:, t, :].unsqueeze(1)
+            gt = None if g is None else g_btc[:, t, :].unsqueeze(1)
+            # print("ct", ct)
+            x = current_input
+            x = self.first_conv.incremental_forward(x)
+            skips = None
+            for f in self.conv_layers:
+                x, h = f.incremental_forward(x, ct, gt)
+                if self.legacy:
+                    skips = h if skips is None else (skips + h) * math.sqrt(0.5)
+                else:
+                    skips = h if skips is None else (skips + h)
+            x = skips
+            for f in self.last_conv_layers:
+                try:
+                    x = f.incremental_forward(x)
+                except AttributeError:
+                    x = f(x)
+
+            # Generate next input by sampling
+            if self.scalar_input:
+                x = sample_from_discretized_mix_logistic(
+                    x.view(B, -1, 1), log_scale_min=log_scale_min)
+            else:
+                x = F.softmax(x.view(B, -1), dim=1) if softmax else x.view(B, -1)
+                if quantize:
+                    sample = np.random.choice(
+                        np.arange(self.out_channels), p=x.view(-1).data.cpu().numpy())
+                    x.zero_()
+                    x[:, sample] = 1.0
+            outputs += [x.data]
+        # T x B x C
+        # outputs = torch.stack(outputs)
+        # B x C x T
+        # outputs = outputs.transpose(0, 1).transpose(1, 2).contiguous()
+
+        self.clear_buffer()
+        return outputs
+
+    def incremental_forward_recover(self, state, length, precision, categoricals_pop, initial_input=None, c=None, g=None,
+                            T=None, test_inputs=None,
+                            tqdm=lambda x: x, softmax=False, quantize=False,
+                            log_scale_min=-7.0):
+        """Incremental forward step
+
+        Due to linearized convolutions, inputs of shape (B x C x T) are reshaped
+        to (B x T x C) internally and fed to the network for each time step.
+        Input of each time step will be of shape (B x 1 x C).
+
+        Args:
+            initial_input (Tensor): Initial decoder input, (B x C x 1)
+            c (Tensor): Local conditioning features, shape (B x C' x T)
+            g (Tensor): Global conditioning features, shape (B x C'' or B x C''x 1)
+            T (int): Number of time steps to generate.
+            test_inputs (Tensor): Teacher forcing inputs (for debugging)
+            tqdm (lamda) : tqdm
+            softmax (bool) : Whether applies softmax or not
+            quantize (bool): Whether quantize softmax output before feeding the
+                network output to input for the next time step. TODO: rename
+            log_scale_min (float):  Log scale minimum value.
+
+        Returns:
+            Tensor: Generated one-hot encoded samples. B x C x T　
+                or scaler vector B x 1 x T
+        """
+        self.clear_buffer()
+        B = 1
+        T=length
+        # Note: shape should be **(B x T x C)**, not (B x C x T) opposed to
+        # batch forward due to linealized convolution
+        if test_inputs is not None:
+            if self.scalar_input:
+                if test_inputs.size(1) == 1:
+                    test_inputs = test_inputs.transpose(1, 2).contiguous()
+            else:
+                if test_inputs.size(1) == self.out_channels:
+                    test_inputs = test_inputs.transpose(1, 2).contiguous()
+
+            B = test_inputs.size(0)
+            if T is None:
+                T = test_inputs.size(1)
+            else:
+                T = max(T, test_inputs.size(1))
+        # cast to int in case of numpy.int64...
+        T = int(T)
+
+        # Global conditioning
+        if g is not None:
+            if self.embed_speakers is not None:
+                g = self.embed_speakers(g.view(B, -1))
+                # (B x gin_channels, 1)
+                g = g.transpose(1, 2)
+                assert g.dim() == 3
+    #     g_btc = _expand_global_features(B, T, g, bct=False)
+
+        # Local conditioning
+        if c is not None and self.upsample_conv is not None:
+            # B x 1 x C x T
+            c = c.unsqueeze(1)
+            for f in self.upsample_conv:
+                c = f(c)
+            # B x C x T
+            c = c.squeeze(1)
+            print("c.size()", c.size())
+            print("T", T)
+            assert c.size(-1) == T
+        if c is not None and c.size(-1) == T:
+            c = c.transpose(1, 2).contiguous()
+
+        outputs = []
+        if initial_input is None:
+            if self.scalar_input:
+                initial_input = torch.zeros(B, 1, 1)
+            else:
+                initial_input = torch.zeros(B, 1, self.out_channels)
+                initial_input[:, :, 127] = 1  # TODO: is this ok?
+            # https://github.com/pytorch/pytorch/issues/584#issuecomment-275169567
+            if next(self.parameters()).is_cuda:
+                initial_input = initial_input.cuda()
+        else:
+            if initial_input.size(1) == self.out_channels:
+                initial_input = initial_input.transpose(1, 2).contiguous()
+
+        current_input = initial_input
+        recovered_data = np.zeros(length, dtype=np.int32)
+        all_probs = []
+        for t in tqdm(range(length)):
+            if t % 40 == 0:
+                torch.save(all_probs, "lik_pop_probs.pt")
+                print("iteration", t)
+            if test_inputs is not None and t < test_inputs.size(1):
+                current_input = test_inputs[:, t, :].unsqueeze(1)
+            else:
+                if t > 0:
+                    current_input_scalar = recovered_data[t-1]
+                    current_input = torch.zeros(1, 256)
+                    current_input[0, current_input_scalar] = 1.
+                    
 
             # Conditioning features for single time step
             ct = None if c is None else c[:, t, :].unsqueeze(1)
@@ -359,13 +515,24 @@ class WaveNet(nn.Module):
                     x.zero_()
                     x[:, sample] = 1.0
             outputs += [x.data]
+            probs = F.softmax(outputs[-1], dim=-1).numpy()
+            all_probs.append(probs)
+            try:
+                state, scalar_input = categoricals_pop(probs, precision)(state)
+                print(scalar_input.item())
+            except Exception as e:
+                print("e", e)
+                print("state", state)
+            scalar_input = scalar_input[0]
+            recovered_data[t] = scalar_input
+
         # T x B x C
         outputs = torch.stack(outputs)
         # B x C x T
         outputs = outputs.transpose(0, 1).transpose(1, 2).contiguous()
 
         self.clear_buffer()
-        return outputs
+        return state, recovered_data, all_probs
 
     def clear_buffer(self):
         self.first_conv.clear_buffer()
